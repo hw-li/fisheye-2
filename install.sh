83be7ab84506593fd2bd8fb88a36e0e3005919cd
dell xps 15 <zleizju@gmail.com>
2018-03-16 13:45:04 -0700

    add install bash

---
+++ install.sh
@@ -0,0 +1,16 @@
+git clone https://github.com/lanzhige/fisheye.git
+cd fisheye
+mkdir -p ./build
+cd ./build
+cmake -DCMAKE_BUILD_TYPE=Release ..
+cmake --build .
+cd ..
+cd ..
+rm -rf ./output
+mkdir -p ./output
+cp ./fisheye/build/Release/fisheye.exe ./output
+cp ./fisheye/bin/zlib.dll ./output
+mkdir -p ./output/shaders
+cp ./fisheye/shaders/* ./output/
+cp ./fisheye/README.md ./output/
+rm -rf ./fisheye

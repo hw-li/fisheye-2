8008eac35bc9d611cac4653788b633fd5e76baa4
Lei Zhang (Student) <lzhan253@sundevils.asu.edu>
2018-03-16 14:51:23 -0700

    bash changes

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # fisheye
 Create fisheye image and binary output.
+
+Instructions:
+In command line:
+# to generate fisheye image:
+fisheye.exe {INPUT_DIR} {OUTPUT_DIR}
+# to generate compressed binary files:
+fisheye.exe -b {INPUT_DIR} {OUTPUT_DIR}
--- install.sh
+++ install.sh
@@ -2,15 +2,13 @@ git clone https://github.com/lanzhige/fisheye.git
 cd ./fisheye
 mkdir -p ./build
 cd ./build
-cmake -DCMAKE_BUILD_TYPE=Release ..
-cmake --build .
+cmake ..
+cmake --build . --config Release
 cd ..
 cd ..
-rm -rf ./output
-mkdir -p ./output
-cp ./fisheye/build/Release/fisheye.exe ./output
-cp ./fisheye/bin/zlib.dll ./output
-mkdir -p ./output/shaders
-cp ./fisheye/shaders/* ./output/shaders/
-cp ./fisheye/README.md ./output/
+cp ./fisheye/build/src/Release/fisheye.exe ./
+cp ./fisheye/bin/zlib.dll ./
+mkdir -p ./shaders
+cp ./fisheye/shaders/* ./shaders/
+cp ./fisheye/README.md ./
 rm -rf ./fisheye

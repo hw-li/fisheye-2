e3f2d442ce555918c6807127833f4dc2df86971c
Lei Zhang (Student) <lzhan253@sundevils.asu.edu>
2018-03-16 14:17:32 -0700

    change bash

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/lanzhige/fisheye.git
-cd fisheye
+cd ./fisheye
 mkdir -p ./build
 cd ./build
 cmake -DCMAKE_BUILD_TYPE=Release ..
@@ -11,6 +11,6 @@ mkdir -p ./output
 cp ./fisheye/build/Release/fisheye.exe ./output
 cp ./fisheye/bin/zlib.dll ./output
 mkdir -p ./output/shaders
-cp ./fisheye/shaders/* ./output/
+cp ./fisheye/shaders/* ./output/shaders/
 cp ./fisheye/README.md ./output/
 rm -rf ./fisheye
